c788d94877778fc0675b082641d39ce57c0a441e
RickDeb2004 <debanjanrick04@gmail.com>
2024-02-06 19:55:37 +0530

    feat:: added repeatition

---
+++ Repetition.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+   string s;
+   cin>>s;
+   int ans=0, c=0;
+   char l='A';
+   for(char d: s){
+    if(d==l){
+        ++c;
+        ans=max(ans,c);
+    }
+    else{
+     l=d;
+     c=1;
+    }
+   }
+   cout<<ans;
+   }
